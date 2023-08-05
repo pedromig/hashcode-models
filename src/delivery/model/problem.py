e10c9b67ab4e97e8a0cef209f4cd0409f82ae81b
Pedro Rodrigues <pedromiguelrodrigues2000@gmail.com>
2023-08-05 17:48:21 +0100

    Removing unwanted files

--- src/delivery/model/problem.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-from typing import TYPE_CHECKING, Iterable
-
-if TYPE_CHECKING:
-  from . import *
-
-class Problem:
-  def __init__(self: Problem) -> None:
-    ... 
